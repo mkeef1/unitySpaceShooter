7b6034f41b21f2743618dfa554774c3707ff001e
Matt Keef <mkeef1@gmail.com>
2015-01-23 00:30:40 -0600

    asteroid waves, explosion resources destroyed

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,14 +5,25 @@ public class GameController : MonoBehaviour {
 
   public GameObject hazard;
   public Vector3 spawnValues;
+  public int hazardCount;
+  public float spawnWait;
+  public float startWait;
+  public float waveWait;
 
   void Start(){
-    SpawnWaves();
+    StartCoroutine(SpawnWaves());
   }
 
-  void SpawnWaves(){
-    Vector3 spawnPostion = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-    Quaternion spawnRotation = Quaternion.identity;
-    Instantiate(hazard, spawnPostion, spawnRotation);
+  IEnumerator SpawnWaves(){
+    yield return  new WaitForSeconds(startWait);
+    while(true){
+      for(int i = 0; i < hazardCount; i++){
+        Vector3 spawnPostion = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPostion, spawnRotation);
+        yield return  new WaitForSeconds(spawnWait);
+      }
+      yield return new WaitForSeconds(waveWait);
+    }
   }
 }
